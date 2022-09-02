71033b88b8336e6bc1ce62a7de76e13487aacf84
EC2 Default User <ec2-user@ip-172-31-82-182.ec2.internal>
2022-09-02 00:50:15 +0000

    main.tf Commit

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "region" {
+  default     = "us-east-1"
+  description = "AWS Region"
+}
+
+variable "ami" {
+  default     = "ami-090fa75af13c156b4"
+  description = "Amazon Machine Image ID for Ubuntu Server 20.04"
+}
+
+variable "type" {
+  default     = "t2.micro"
+  description = "Size of VM"
+}
+
+resource "aws_instance" "Web" {
+  ami           = var.ami
+  instance_type = var.type
+  tags = {
+    Name = "Pipeline-EC2"
+  }
+}
+
+output "ec2_instance_publicip" {
+  description = "EC2 Instance Public IP"
+  value       = aws_instance.Web.public_ip
+}
+
+output "Web_EC2_Public_DNS" {
+  description = "Public DNS URL of an EC2 Instance"
+  value       = "http://${aws_instance.Web.public_dns}"
+}
